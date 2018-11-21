f0b36457d8bc8aa1da15c5f21eab64c029a31460
Daniel Porta <daniel.porta@dfki.de>
2018-11-21 22:45:21 +0100

    Mir REST component added

--- devices/de.dfki.cos.basys.demonstrator.device.mirrest/src/main/java/de/dfki/iui/basys/demonstrator/device/mirrest/MirRestComponent.java
+++ devices/de.dfki.cos.basys.demonstrator.device.mirrest/src/main/java/de/dfki/iui/basys/demonstrator/device/mirrest/MirRestComponent.java
@@ -1,5 +1,16 @@
 package de.dfki.iui.basys.demonstrator.device.mirrest;
 
+
+import de.dfki.cos.basys.common.mirrestclient.MiRState;
+import de.dfki.cos.basys.common.mirrestclient.MirRestClient;
+import de.dfki.cos.basys.common.mirrestclient.MirRestClientImpl;
+import de.dfki.cos.basys.common.mirrestclient.dto.MissionInstanceInfo;
+import de.dfki.cos.basys.common.mirrestclient.dto.Status;
+import de.dfki.iui.basys.model.domain.capability.CapabilityPackage;
+import de.dfki.iui.basys.model.domain.capability.MoveToLocation;
+import de.dfki.iui.basys.model.domain.resourceinstance.CapabilityVariant;
+import de.dfki.iui.basys.model.domain.resourceinstance.LogisticsCapabilityVariant;
+import de.dfki.iui.basys.model.domain.topology.TopologyElement;
 import de.dfki.iui.basys.model.runtime.component.CapabilityRequest;
 import de.dfki.iui.basys.model.runtime.component.ComponentConfiguration;
 import de.dfki.iui.basys.model.runtime.component.ResponseStatus;
@@ -10,25 +21,37 @@ import de.dfki.iui.basys.runtime.component.device.packml.UnitConfiguration;
 
 public class MirRestComponent extends DeviceComponent {	
 	
+	MirRestClient client = null;
+	MissionInstanceInfo currentMission = null;
+	
 	public MirRestComponent(ComponentConfiguration config) {
 		super(config);
 	}
 	
 	@Override
+	public void connectToExternal() throws ComponentException {
+		super.connectToExternal();
+		client = new MirRestClientImpl(getConfig().getExternalConnectionString(),getConfig().getProperty("auth").getValue());
+	}
+	
+	@Override
 	protected UnitConfiguration translateCapabilityRequest(CapabilityRequest req) {
 
-		//CapabilityVariant<?> variant = (CapabilityVariant<?>)req.getCapabilityVariant();
-		// c = variant.getCapability();
+		UnitConfiguration config = new UnitConfiguration();
 
-		//if (c.eClass().equals(CapabilityPackage.eINSTANCE.getSwitchConfirmationCapability()))
-		{
-			//return new UnitConfiguration();
+		CapabilityVariant<?> c = req.getCapabilityVariant();
+		TopologyElement te = null;
+		if (c.getCapability().eClass().equals(CapabilityPackage.eINSTANCE.getMoveToLocation())) {
+			te = ((MoveToLocation) c.getCapability()).getTargetLocation();
 		}
-		return new UnitConfiguration();
-		//throw new ComponentException("unknown capability");
-		//return null;
-		
 
+		if (c.getCapability().eClass().equals(CapabilityPackage.eINSTANCE.getTransport())) {
+			LogisticsCapabilityVariant variant = (LogisticsCapabilityVariant) c;
+			te = variant.getAppliedOn().get(0);
+		}
+
+		config.setPayload(te);
+		return config;
 	}
 	
 	@Override
@@ -39,34 +62,83 @@ public class MirRestComponent extends DeviceComponent {
 	}
 
 
+	@Override
+	public void onResetting() {
+		Status status = client.setRobotStatus(MiRState.READY);
+		currentMission = null;
+	}
 	
 	@Override
-	public void onStarting() {
-		
-
+	public void onStarting() {		
+		TopologyElement targetElement = ((TopologyElement) getUnitConfig().getPayload());
+		LOGGER.info("Moving to position: " + targetElement.getName());
+		try {
+			currentMission = client.gotoSymbolicPosition(targetElement.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+			stop();
+		}
 	}
 	
 	@Override
 	public void onExecute() {
-		
+		try {
+			boolean executing = true;
+			while(executing) {
+				currentMission = client.getMissionInstanceInfo(currentMission.id);
+				LOGGER.debug("MissionState is " + currentMission.state);
+				 
+				switch (currentMission.state.toLowerCase()) {
+				case "pending":
+					break;
+				case "executing":
+					break;
+				case "finished":
+					executing=false;
+					break;
+				case "failed":
+					executing= false;
+					setErrorCode(1);
+					stop();
+					break;
+				default:
+					break;
+				}
+	
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			setErrorCode(3);
+			stop();
+		}
 		
 	}
 
 	@Override
-	public void onCompleting() {
-		
-		//TODO: notify Basys
-		//outChannel.sendMessage("");
+	public void onCompleting() {		
 		sendComponentResponse(ResponseStatus.OK, 0);
 	}
 	
 	@Override
 	public void onStopping() {
+		sendComponentResponse(ResponseStatus.NOT_OK, getErrorCode());
+		try {
+			Status status = client.setRobotStatus(MiRState.PAUSED);
+			if (currentMission != null) {
+				client.dequeueMissionInstance(currentMission.id);
+				currentMission = null;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	@Override
-	public void onResetting() {
-	}
 	
 	
 }
